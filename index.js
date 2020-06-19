38241172e33ede9b4ab703a8c931c996f3e82f92
amanjagdev <amankumarjagdev@gmail.com>
2020-06-19 12:46:59 +0530

    Error,Warning and Info Logs completed

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
 const colors = require('colors');
 
-const autoLog = () => {
-    console.log(this);
-} 
+//Error Log
+const autoLogE = (message) => {
 
-module.exports.autoLog = autoLog;
+    //Creating Error to get name of function
+    let stack = new Error().stack
+    let caller = stack.split('\n')[2].trim();
+    caller = caller.slice(3, caller.length)
+    let arr = caller.split(" ");
+
+    const nameOfFunction = arr[0];
+
+    console.log(`${nameOfFunction}`.blue + ` |`.green + ` Error: ${message}`.red.bold);
+}
+
+//Info Log
+const autoLogI = (message) => {
+
+    //Creating Error to get name of function
+    let stack = new Error().stack
+    let caller = stack.split('\n')[2].trim();
+    caller = caller.slice(3, caller.length)
+    let arr = caller.split(" ");
+
+    const nameOfFunction = arr[0];
+
+    console.log(`${nameOfFunction}`.blue + ` |`.green + ` Info: ${message}`.blue.bold);
+}
+
+
+//Warning Log
+const autoLogW = (message ) => {
+
+    //Creating Error to get name of function
+    let stack = new Error().stack
+    let caller = stack.split('\n')[2].trim();
+    caller = caller.slice(3, caller.length)
+    let arr = caller.split(" ");
+
+    const nameOfFunction = arr[0];
+
+    console.log(`${nameOfFunction}`.blue + ` |`.green + ` Warning: ${message}`.yellow.bold);
+}
+
+module.exports.autoLog = autoLogE;
+module.exports.autoLogE = autoLogE;
+module.exports.autoLogI = autoLogI;
+module.exports.autoLogW = autoLogW;
